7ce9c6474f9729442d190fb78c8386cea077fe1b
jpst-dev <jpst_2001@hotmail.com>
2026-04-14 19:21:28 -0300

    feature: hide filter option

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }
+      ]
+    }
+  },
   modules: ['@nuxt/ui', '@pinia/nuxt'],
   icon: {
     serverBundle: {
